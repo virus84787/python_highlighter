f96dd8b45045747ff2658114f789b2e2420a6b84
Vadym Levchenko <levchenko.vadym@gmail.com>
2019-06-17 09:15:21 +0300

    home work

--- highlighter/__init__.py
+++ highlighter/__init__.py
@@ -5,7 +5,7 @@ author smith@example.com
 license: MIT"""
 
 from flask import Flask, render_template, request, Markup
-
+import re
 
 def create_app():
     """Create flask app for binding."""
@@ -14,14 +14,15 @@ def create_app():
     template_file_name = 'index.html'
 
     @app.route('/', methods=['GET'])
-    def index():
+    def index(): #pylint: disable=w0612
         return render_template(template_file_name)
 
     @app.route('/', methods=['POST'])
-    def process():
+    def process():#pylint: disable=w0612
         search_text = request.form['search']
+        case_sensitive = int(request.form['case_sensitive'])
         text = request.form['text']
-        highlighted_text = highlight_text(text, search_text)
+        highlighted_text = highlight_text(text, search_text, case_sensitive)
         result = {'text': text,
                   'highlighted_text': Markup(highlighted_text),
                   }
@@ -35,14 +36,23 @@ def create_app():
         result = "<mark>" + text + "</mark>"
         return result
 
-    def highlight_text(text, expr):
+    def highlight_text(text, expr, case_sensitive):
         """Markup searched string in given text.
         @:param text - string text to be processed (e.g., 'The sun in the sky')
         @:param expr - string pattern to be searched in the text (e.g., 'th')
         @:return marked text, e.g., "<mark>Th</mark>e sun in <mark>th</mark>e sky"."""
-        if expr in text:
-            text = text.replace(expr, markup_text(expr))
+#         if expr in text:
+#             text = text.replace(expr, markup_text(expr))
+#         result = text
+#         return result
+        if case_sensitive == 0:
+            if expr in text:
+                text = text.replace(expr, markup_text(expr))
+        elif case_sensitive == 1:
+            if expr.lower() in text.lower():
+                insensitive_hippo = re.compile(re.escape(expr), re.IGNORECASE)
+                text = insensitive_hippo.sub(markup_text(expr), text)
         result = text
-        return result
+        return result        
 
     return app
